990099ffe18f9fa48903d5452387485c86592719
Barder_Lucas <bardey.estelle1@gmail.com>
2025-01-10 14:27:45 +0100

    html pages Règles et FAQ

--- src/pages/faq.vue
+++ src/pages/faq.vue
@@ -4,4 +4,48 @@
 </script>
 <template>
     <h1>Foire aux questions</h1>
+    <nav class="flex flex-wrap gap-5">
+        <a href="#event">
+            <button>
+                <p>L'événement</p>
+            </button>
+        </a>
+        <a href="#tournoi">
+            <button>
+                <p>Le tournoi</p>
+            </button>
+        </a>
+        <a href="#accessible">
+            <button>
+                <p>L'accessibilité</p>
+            </button>
+        </a>
+        <a href="#benevole">
+            <button>
+                <p>Le bénévolat</p>
+            </button>
+        </a>
+    </nav>
+    <article id="event">
+        <h2>L'évenement</h2>
+        <!--Cards-->
+    </article>
+    <article id="tournoi">
+        <h2>Le tournois</h2>
+        <!--Cards-->
+    </article>
+    <article id="accessible">
+        <h2>L'accessibilité</h2>
+        <!--Cards-->
+    </article>
+    <article id="benevole">
+        <h2>Le bénévolat</h2>
+        <!--Cards-->
+    </article>
+    <p>Vous avez encore des questions sans réponse ?<br/>Envoyez nous un message via nos réseaux sociaux ou depuis notre formulaire de contact !</p>
+    <RouterLink to="/contact">
+        <button>
+            <p>Nous contacter</p>
+        </button>
+    </RouterLink>
 </template>
--- src/pages/regles.vue
+++ src/pages/regles.vue
@@ -4,19 +4,33 @@
 </script>
 <template>
     <h1>Régles</h1>
-    <a>
-        <button>
-            <p>Les régles générales</p>
-        </button>
-    </a>
-    <a>
-        <button>
-            <p></p>
-        </button>
-    </a>
-    <a>
-        <button>
-            <p></p>
-        </button>
-    </a>
+    <nav class="flex gap-5 flex-wrap">
+        <a href="#rGenerales">
+            <button>
+                <p>Les régles générales</p>
+            </button>
+        </a>
+        <a href="#rTournois">
+            <button>
+                <p>Règles autour du tournoi</p>
+            </button>
+        </a>
+        <a href="#rAutres">
+            <button>
+                <p>Autres règles</p>
+            </button>
+        </a>
+    </nav>
+    <article id="rGenerales">
+        <h2>Les règles générales</h2>
+        <!--Cards-->
+    </article>
+    <article id="rTournois">
+        <h2>Les règles autour du tournois</h2>
+        <!--Cards-->
+    </article>
+    <article id="rAutres">
+        <h2>Autres Régles</h2>
+        <!--Cards-->
+    </article>
 </template>
