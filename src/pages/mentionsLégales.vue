61b5f0eebf59a63de85b514293d93bcb95fb0df6
Barder_Lucas <bardey.estelle1@gmail.com>
2025-01-10 15:13:20 +0100

    html page mentions légales

--- src/pages/mentionsLégales.vue
+++ src/pages/mentionsLégales.vue
@@ -4,4 +4,30 @@
 </script>
 <template>
     <h1>Mentions légales</h1>
+    <section>
+        <h2>Éditeur du site</h2>
+        <p>Nom de l'association : Smash Arena Event</p>
+        <p>Forme juridique : Association à but non lucratif</p>
+        <p>Siège social : <a href="https://www.google.com/maps/place/28+Fbg+de+Besan%C3%A7on,+25200+Montb%C3%A9liard/@47.5091913,6.7905298,17z/data=!3m1!4b1!4m6!3m5!1s0x479216eed7a8c77f:0xe6a34cece8fd2ab!8m2!3d47.5091913!4d6.7905298!16s%2Fg%2F11csj39t0b?authuser=0&entry=ttu&g_ep=EgoyMDI1MDEwNy4wIKXMDSoASAFQAw%3D%3D">28 Faubourg de Besançon</a></p>
+        <p>Numéro de téléphone : <a href="tel:+0638881975">06 38 88 19 75</a></p>
+        <p>Adresse e-mail : <a href="mailto:smasharenaevent@gmail.com">smasharenaevent@gmail.com</a></p>
+        <p>Numéro d'immatriculation : W252007118</p>
+    </section>
+    <section>
+        <h2>Directeur de la publication</h2>
+        <p>Nom du directeur : Valentin SARAIVA</p>
+        <p>Contact : <a href="mailto:valentinsa0701@gmail.com">valentinsa0701@gmail.com</a></p>
+    </section>
+    <section>
+        <h2>Hébergeur du site</h2>
+        <p>Nom de l'hébergeur : Netlify</p>
+        <p>Adresse : <a href="https://www.google.com/maps/place/Netlify/@37.7827092,-122.3935642,19z/data=!3m1!4b1!4m6!3m5!1s0x808f7fb9873cc0db:0x227d185e8e05e363!8m2!3d37.7827092!4d-122.3935642!16s%2Fg%2F11b76gvzx1?authuser=0&entry=ttu&g_ep=EgoyMDI1MDEwNy4wIKXMDSoASAFQAw%3D%3D">512 2nd Street, Suite 200 San Francisco, CA 94107, États-Unis</a></p>
+        <p>Contact : <a href="mailto:support@netlify.com">support@netlify.com</a></p>
+    </section>
+    <section>
+        <h2>Propriété<br/>intellectuelle</h2>
+        <p>Les illustrations des personnages sur la page d'accueil sont réalisées par l'artiste ElevenZM, dont les droits d'auteur sont réservés. Toute reproduction ou utilisation non autorisée est interdite. L’utilisation de ses illustrations se fait sur la mention de l’artiste dans nos mentions légales.</p>
+        <p>Lien de sa page sur Deviant Art :</p>
+        <p><a href="https://www.deviantart.com/elevenzm/gallery">https://www.deviantart.com/elevenzm/gallery.</a></p>
+    </section>
 </template>
