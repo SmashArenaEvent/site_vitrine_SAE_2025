fb8cd4448cd4770d3cc5e3d948f810af310227fc
Barder_Lucas <bardey.estelle1@gmail.com>
2025-01-10 16:02:57 +0100

    Coloration des text page index

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,7 +5,7 @@
 </script>
 <template>
     <section>
-        <h1>Smash arena event <span>2ème édition</span></h1>
+        <h1><span class="text-Tonic">S</span>mash <span class="text-Tonic">a</span>rena <span class="text-Tonic">e</span>vent <span  class="text-Tonic">2ème édition</span></h1>
         <p>Bienvenue au Smash Arena Event ! Nous sommes une association étudiante organisant des <span>tournois gratuits</span> entre membre du Nord Franche-Comté sur le jeu <span>Super Smash Bros Ultimate</span> et bien plus !</p>
         <a href="#">
             <button>
@@ -16,13 +16,13 @@
     </section>
     <section>
         <h2>
-            <span>Smash Arena Event</span> Première édition
+            <span class="text-Tonic">Smash Arena Event</span> Première édition
         </h2>
         <!--Carousel-->
     </section>
     <section>
         <h2>
-            <span>Les règles</span> principales
+            <span class="text-Tonic">Les règles</span> principales
         </h2>
         <!--Cards-->
         <RouterLink to="/regles">
@@ -32,7 +32,7 @@
         </RouterLink>
     </section>
     <section>
-        <h2><span>Le coin</span> chill</h2>
+        <h2><span class="text-Tonic">Le coin</span> chill</h2>
         <!--Image-->
         <div>
             <p>Le Coin Chill, c’est un espace détente, pour se reposer entre 2 match du tournois et les amis juste venus s’amuser.<br/><br/>De nombreux jeux y sont disponible en libre service :</p>
@@ -53,24 +53,24 @@
         </div>
     </section>
     <section>
-        <h2><span>Le coin</span> buvette</h2>
+        <h2><span class="text-Tonic">Le coin</span> buvette</h2>
         <!--Image-->
         <div>
             <p>La Buvette, nécessaire pour se restaurer durant un après-midi à s’amuser ! Mais attention, à consommer dans l’espace buvette, interdiction de manger à l’espace du Tournoi ou au coin Chill.</p>
         </div>
     </section>
     <section>
-        <h2><span>Tu veux participer ?</span>Inscris-toi !</h2>
+        <h2><span  class="text-Tonic">Tu veux participer ?</span>Inscris-toi !</h2>
         <!--Section choix participation-->
     </section>
     <section>
-        <h2><span>Nos</span> Sponsors</h2>
+        <h2><span class="text-Tonic">Nos</span> Sponsors</h2>
         <div>
             <!--Ajout des sponsors-->
         </div>
     </section>
     <section>
-        <h2><span>Nous</span> contacter</h2>
+        <h2><span class="text-Tonic">Nous</span> contacter</h2>
         <Formulaire/>
     </section>
 </template>
