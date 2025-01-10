f3bfd7fa21506ed24596f2983e31de5a2060cea2
Barder_Lucas <bardey.estelle1@gmail.com>
2025-01-10 14:51:26 +0100

    Html page à propos

--- src/pages/aPropos.vue
+++ src/pages/aPropos.vue
@@ -1,7 +1,38 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
+import Formulaire from '@/components/Formulaire.vue';
+
     
 </script>
 <template>
-    <h1>à propos</h1>
+    <article>
+        <h1>à propos</h1>
+        <p>Lancé pour la première fois le 10 février 2024,<br/>Smash Arena Event est bien plus qu'un simple tournoi :<br/>c’est une initiative ambitieuse portée par des étudiants motivés de l’IUT de Montbéliard.</p>
+        <p>Ce rassemblement convivial autour de Super Smash Bros Ultimate a réuni 30 participants en compétition dans un tournoi double élimination, marqué par la victoire de Mathis Regazzoni.</p>
+        <p>Au total, ce sont près de 70 visiteurs qui ont répondu présents pour la première édition, curieux de découvrir une expérience mêlant jeu, détente et passion.</p>
+        <!--Image-->
+        <h2>Une expérience immersive</h2>
+        <p>Pour rythmer la journée, plusieurs espaces avaient été mis en place pendant l’événement :</p>
+        <p class="indent-5">Le coin chill avec des jeux comme Mario Kart, Yu-Gi-Oh!, Splatoon et des jeux de société (UNO, Gallion...).</p>
+        <p class="indent-5">Une zone de restauration pour s'accorder un moment de convivialité entre deux matchs. </p>
+        <!--Image-->
+        <h2>Un succès prometteur</h2>
+        <p>Cette première édition a permis de poser des bases solides, tant en termes d’organisation que de participation. Grâce à l’implication des bénévoles et des organisateurs, Smash Arena Event s’est imposé comme un rendez-vous incontournable pour les passionnés de jeux vidéo.</p>
+        <p>Alors ? Est-ce que vous êtes prêt pour la Deuxième Édition ?</p>
+    </article>
+    <article>
+        <h2>Notre équipe</h2>
+        <section>
+            <h3>Première édition</h3>
+            <!--Card Porfil-->
+        </section>
+        <section>
+            <h3>Seconde édition</h3>
+            <!--Card Porfil-->
+        </section>
+    </article>
+    <section>
+        <h2>Nous contacter</h2>
+        <Formulaire/>
+    </section>
 </template>
