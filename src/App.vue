c43a366147cf1c748ffaf8259bf5a5a4cddbb3da
Barder_Lucas <bardey.estelle1@gmail.com>
2025-01-10 09:32:39 +0100

    Intéraction header et visuelles place holder

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,14 @@ import LoginOut from './components/LoginOut.vue';
 import Header from '@/components/Header.vue';
 </script>
 <template>
-  <Header/>
-  <main>
+  <Header class="bg-slate-950 h-screen text-white" />
+  <main class="bg-slate-950 h-screen text-white">
     <RouterView v-slot="{ Component }">
       <Suspense>
         <component :is="Component" :key="$route.path" />
       </Suspense>
     </RouterView>
+    <Footer class="bg-slate-950 h-screen text-white"/>
   </main>
 
 </template>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,14 +4,16 @@
     import Menu from './icons/Menu.vue';
     import Croix from './icons/Croix.vue';
     
+    import { ref } from 'vue';
     import { RouterLink } from 'vue-router';
     import { useRoute } from 'vue-router';
     const route = useRoute();
+    const activeMenu = ref(false)
 </script>
 <template>
-    <header class="flex">
+    <header class="flex w-screen justify-between">
         <Logo/>
-        <nav>
+        <nav class="hidden" :class="{'!block':activeMenu}">
             <button>
                 <p>Règles</p>
             </button>
@@ -25,7 +27,7 @@
                 <p>Contact</p>
             </button>
         </nav>
-        <Menu/>
-        <Croix/>
+        <Menu class="block" @click="activeMenu =! activeMenu" :class="{'!hidden':activeMenu}"/>
+        <Croix class="hidden" @click="activeMenu =! activeMenu" :class="{'!block':activeMenu}"/>
     </header>
 </template>
