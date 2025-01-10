5cc416dce808c01369f39bef23d5cea936c071d0
Barder_Lucas <bardey.estelle1@gmail.com>
2025-01-10 15:44:22 +0100

    Politique de confidentialité

--- src/pages/PolitiqueDeConfidentialité.vue
+++ src/pages/PolitiqueDeConfidentialité.vue
@@ -4,4 +4,40 @@
 </script>
 <template>
     <h1>Politique de confidentialité</h1>
+    <section>
+        <h2>Données Personneles et RGPD</h2>
+        <p>Les visiteurs du site peuvent s'inscrire en tant que joueur ou bénévole pour le tournoi. Les données personnelles collectées lors de l'inscription, telles que le nom, l'adresse e-mail, et d'autres informations pertinentes, seront utilisées dans le seul but de gérer la participation au tournoi et de contacter les participants.</p>
+        <p>Un registre pour le traitement des données personnelles conforme au RGPD sera maintenu. Les utilisateurs ont le droit d'accéder, de rectifier, de supprimer ou de demander la portabilité de leurs données personnelles. Pour exercer ces droits, les utilisateurs peuvent envoyer un e-mail à l'adresse spécifiée sur la page contact : <a href="mailto:smasharenaevent@gmail.com">smasharenaevent@gmail.com.</a></p>
+    </section>
+    <section>
+        <h2>Inscriptions en tant que joueur ou bénévole</h2>
+        <p>Les inscriptions en tant que joueur utiliseront l'API de tournois Challonge pour stocker les données des joueurs. Les inscriptions en tant que bénévole généreront des données via un formulaire sur le site Jotform. En s'inscrivant, les participants acceptent que ces données soient utilisées dans le cadre du tournoi. Les données ne seront pas partagées, vendues ou louées à des tiers.</p>
+    </section>
+    <section>
+        <h2>Utilisation des données collectées</h2>
+        <p>Les données collectées seront utilisées pour gérer les inscriptions au tournoi, communiquer des informations relatives à l'événement, et faciliter la participation des joueurs et bénévoles. Les données ne seront pas utilisées à des fins commerciales non liées à l'événement.</p>
+    </section>
+    <section>
+        <h2>Sécurité des données</h2>
+        <p>Nous nous engageons à assurer la sécurité des données personnelles collectées et à mettre en place des mesures appropriées pour protéger ces données contre tout accès non autorisé, perte ou divulgation.</p>
+    </section>
+    <section>
+        <h2>Vos Droits</h2>
+        <p>Les utilisateurs ont le droit d'accéder, de rectifier, de supprimer, ou de demander la portabilité de leurs données personnelles. Pour exercer ces droits, les utilisateurs peuvent envoyer un e-mail à l'adresse spécifiée sur notre page contact et demander aux organisateurs à réaliser un changement ou une suppression de leurs données personnelles.</p>
+    </section>
+    <section>
+        <h2>Modifications de la politique de confidentialité</h2>
+        <p>Nous nous réservons le droit de modifier cette politique de confidentialité à tout moment. Les utilisateurs seront informés de ces modifications via nos réseaux sociaux.</p>
+        <p>Facebook : <a href="https://www.facebook.com/profile.php?id=61553864250828">https://www.facebook.com/profile.php?id=61553864250828.</a></p>
+        <p>Twitter : <a href=" https://x.com/SmashArenaEvent">https://x.com/SmashArenaEvent.</a></p>
+        <p>Instagram : <a href="https://www.instagram.com/smash_arena_event/">https://www.instagram.com/smash_arena_event/.</a></p>
+    </section>
+    <section>
+        <h2>Politique de Challonge (API utilisée pour les joueurs)</h2>
+        <p>Challonge est un service tiers utilisé pour gérer les données des joueurs. La politique de confidentialité et les conditions d'utilisation de Challonge peuvent être consultées sur leur site web : <a href="https://challonge.com/privacy_policy">https://challonge.com/privacy_policy.</a></p>
+    </section>
+    <section>
+        <h2>Politique de Jotform (site utilisé pour les bénévoles)</h2>
+        <p>Jotform est un service tiers utilisé pour générer des données à partir des formulaires des bénévoles. La politique de confidentialité et les conditions d'utilisation de Jotform peuvent être consultées sur leur site web :<br/><a href="https://www.jotform.com/privacy">https://www.jotform.com/privacy.</a></p>
+    </section>
 </template>
