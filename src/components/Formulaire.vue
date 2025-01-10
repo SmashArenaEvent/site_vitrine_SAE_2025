3ef6d57965ecd4ef2e975ad129cd37ee2909bb0f
Barder_Lucas <bardey.estelle1@gmail.com>
2025-01-10 13:58:08 +0100

    Mise en place du formulaire de contact

--- src/components/Formulaire.vue
+++ src/components/Formulaire.vue
@@ -1,27 +1,41 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
     <form @submit.prevent="submitForm">
-        <div>
-            <input class="text-black" type="text" name="name" v-model="form.name" placeholder="Nom"/>
+        <div class="flex">
+            <div>
+                <input class="text-black" type="text" name="firstName" v-model="form.firstName" placeholder="Prénom"/>
+            </div>
+            <div>
+                <input class="text-black" type="text" name="name" v-model="form.name" placeholder="Nom"/>
+            </div>
         </div>
         <div>
-            <input class="text-black" type="text" name="email" v-model="form.email" placeholder="Email"/>
+            <input class="text-black" type="email" name="email" v-model="form.email" placeholder="Email"/>
         </div>
         <div>
             <textarea class="text-black" rows="5" name="message" v-model="form.message" placeholder="Message"/>
+        </div>
+        <div>
+            <button><input type="submit" value="Envoyer"/></button>
+            <button><input type="reset" value="Annuler"/></button>
         </div>
     </form>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
+
+const WEB3FORMS_ACCESS_KEY = "2e16caef-865b-4105-80bf-cafccea701be";
+
 interface FormData {
+  firstName: string
   name: string
   email: string
   message: string
 }
 
 const form = ref<FormData>({
+  firstName: '',
   name: '',
   email: '',
   message: ''
@@ -34,16 +48,16 @@ const submitForm = async () => {
       "Content-Type": "application/json",
       Accept: "application/json",
     },
-    // body: JSON.stringify({
-    //   access_key: WEB3FORMS_ACCESS_KEY,
-    //   ...form.value
-    // }),
+    body: JSON.stringify({
+      access_key: WEB3FORMS_ACCESS_KEY,
+      ...form.value
+    }),
   });
   const result = await response.json()
   if (result.success) {
       console.log('Form submitted successfully:', result)
       // Reset the form if needed
-      form.value = { name: '', email: '',  message: '' }
+      form.value = { firstName: '', name: '', email: '',  message: '' }
     } else {
       console.error('Form submission failed:', result)
     }
