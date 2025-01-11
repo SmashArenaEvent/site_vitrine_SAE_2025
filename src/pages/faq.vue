4492a9e362f19a6e662b64b4153a8ddbfac23ae3
Barder_Lucas <bardey.estelle1@gmail.com>
2025-01-11 12:47:41 +0100

    Mise en forme page FAQ

--- src/pages/faq.vue
+++ src/pages/faq.vue
@@ -3,49 +3,49 @@
     
 </script>
 <template>
-    <h1>Foire aux questions</h1>
+    <h1 class="font-fugaz-one uppercase text-4xl"><span class="text-Tonic">F</span>oire <span class="text-Tonic">a</span>ux <span class="text-Tonic">q</span>uestions</h1>
     <nav class="flex flex-wrap gap-5">
-        <a href="#event">
+        <a class="bg-Blanc" href="#event">
             <button>
-                <p>L'événement</p>
+                <p class="text-Noir font-fugaz-one uppercase text-xs">L'événement</p>
             </button>
         </a>
-        <a href="#tournoi">
+        <a class="bg-Blanc" href="#tournoi">
             <button>
-                <p>Le tournoi</p>
+                <p class="text-Noir font-fugaz-one uppercase text-xs">Le tournoi</p>
             </button>
         </a>
-        <a href="#accessible">
+        <a class="bg-Blanc" href="#accessible">
             <button>
-                <p>L'accessibilité</p>
+                <p class="text-Noir font-fugaz-one uppercase text-xs">L'accessibilité</p>
             </button>
         </a>
-        <a href="#benevole">
+        <a class="bg-Blanc" href="#benevole">
             <button>
-                <p>Le bénévolat</p>
+                <p class="text-Noir font-fugaz-one uppercase text-xs">Le bénévolat</p>
             </button>
         </a>
     </nav>
     <article id="event">
-        <h2>L'évenement</h2>
+        <h2 class="font-fugaz-one uppercase text-3xl"><span class="text-Tonic">L'</span>évenement</h2>
         <!--Cards-->
     </article>
     <article id="tournoi">
-        <h2>Le tournois</h2>
+        <h2 class="font-fugaz-one uppercase text-3xl"><span class="text-Tonic">Le</span> tournois</h2>
         <!--Cards-->
     </article>
     <article id="accessible">
-        <h2>L'accessibilité</h2>
+        <h2 class="font-fugaz-one uppercase text-3xl"><span class="text-Tonic">L'</span>accessibilité</h2>
         <!--Cards-->
     </article>
     <article id="benevole">
-        <h2>Le bénévolat</h2>
+        <h2 class="font-fugaz-one uppercase text-3xl"><span class="text-Tonic">Le</span> bénévolat</h2>
         <!--Cards-->
     </article>
     <p>Vous avez encore des questions sans réponse ?<br/>Envoyez nous un message via nos réseaux sociaux ou depuis notre formulaire de contact !</p>
     <RouterLink to="/contact">
-        <button>
-            <p>Nous contacter</p>
+        <button class="bg-Tonic">
+            <p class="text-Noir font-fugaz-one uppercase text-xs">Nous contacter</p>
         </button>
     </RouterLink>
 </template>
