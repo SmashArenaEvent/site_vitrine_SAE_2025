3ca03c0db8c7df46048dcf42d6fd2e4f19c35773
Barder_Lucas <bardey.estelle1@gmail.com>
2025-01-11 16:13:37 +0100

    Début de mise en forme page mentions légales

--- src/pages/mentionsLégales.vue
+++ src/pages/mentionsLégales.vue
@@ -3,31 +3,31 @@
     
 </script>
 <template>
-    <h1>Mentions légales</h1>
+    <h1 class="font-fugaz-one uppercase text-4xl"><span class="text-Tonic">M</span>entions <span class="text-Tonic">l</span>égales</h1>
     <section>
-        <h2>Éditeur du site</h2>
-        <p>Nom de l'association : Smash Arena Event</p>
-        <p>Forme juridique : Association à but non lucratif</p>
-        <p>Siège social : <a href="https://www.google.com/maps/place/28+Fbg+de+Besan%C3%A7on,+25200+Montb%C3%A9liard/@47.5091913,6.7905298,17z/data=!3m1!4b1!4m6!3m5!1s0x479216eed7a8c77f:0xe6a34cece8fd2ab!8m2!3d47.5091913!4d6.7905298!16s%2Fg%2F11csj39t0b?authuser=0&entry=ttu&g_ep=EgoyMDI1MDEwNy4wIKXMDSoASAFQAw%3D%3D">28 Faubourg de Besançon</a></p>
-        <p>Numéro de téléphone : <a href="tel:+0638881975">06 38 88 19 75</a></p>
-        <p>Adresse e-mail : <a href="mailto:smasharenaevent@gmail.com">smasharenaevent@gmail.com</a></p>
-        <p>Numéro d'immatriculation : W252007118</p>
+        <h2 class="font-fugaz-one uppercase text-3xl"><span class="text-Tonic">Éditeur</span> du site</h2>
+        <p>Nom de l'association : <span class="text-Tonic">Smash Arena Event</span></p>
+        <p>Forme juridique : <span class="text-Tonic">Association à but non lucratif</span></p>
+        <p>Siège social : <a href="https://www.google.com/maps/place/28+Fbg+de+Besan%C3%A7on,+25200+Montb%C3%A9liard/@47.5091913,6.7905298,17z/data=!3m1!4b1!4m6!3m5!1s0x479216eed7a8c77f:0xe6a34cece8fd2ab!8m2!3d47.5091913!4d6.7905298!16s%2Fg%2F11csj39t0b?authuser=0&entry=ttu&g_ep=EgoyMDI1MDEwNy4wIKXMDSoASAFQAw%3D%3D"><span class="text-Tonic underline underline-offset-2">28 Faubourg de Besançon</span></a></p>
+        <p>Numéro de téléphone : <a href="tel:+0638881975"><span class="text-Tonic underline underline-offset-2">06 38 88 19 75</span></a></p>
+        <p>Adresse e-mail : <a href="mailto:smasharenaevent@gmail.com"><span class="text-Tonic underline underline-offset-2">smasharenaevent@gmail.com</span></a></p>
+        <p>Numéro d'immatriculation : <span class="text-Tonic">W252007118</span></p>
     </section>
     <section>
-        <h2>Directeur de la publication</h2>
-        <p>Nom du directeur : Valentin SARAIVA</p>
-        <p>Contact : <a href="mailto:valentinsa0701@gmail.com">valentinsa0701@gmail.com</a></p>
+        <h2 class="font-fugaz-one uppercase text-3xl"><span class="text-Tonic">Directeur</span> de la publication</h2>
+        <p>Nom du directeur : <span class="text-Tonic">Valentin SARAIVA</span></p>
+        <p>Contact : <a href="mailto:valentinsa0701@gmail.com"><span class="text-Tonic underline underline-offset-2">valentinsa0701@gmail.com</span></a></p>
     </section>
     <section>
-        <h2>Hébergeur du site</h2>
-        <p>Nom de l'hébergeur : Netlify</p>
-        <p>Adresse : <a href="https://www.google.com/maps/place/Netlify/@37.7827092,-122.3935642,19z/data=!3m1!4b1!4m6!3m5!1s0x808f7fb9873cc0db:0x227d185e8e05e363!8m2!3d37.7827092!4d-122.3935642!16s%2Fg%2F11b76gvzx1?authuser=0&entry=ttu&g_ep=EgoyMDI1MDEwNy4wIKXMDSoASAFQAw%3D%3D">512 2nd Street, Suite 200 San Francisco, CA 94107, États-Unis</a></p>
-        <p>Contact : <a href="mailto:support@netlify.com">support@netlify.com</a></p>
+        <h2 class="font-fugaz-one uppercase text-3xl"><span class="text-Tonic">Hébergeur</span> du site</h2>
+        <p>Nom de l'hébergeur : <span class="text-Tonic">Netlify</span></p>
+        <p>Adresse : <a href="https://www.google.com/maps/place/Netlify/@37.7827092,-122.3935642,19z/data=!3m1!4b1!4m6!3m5!1s0x808f7fb9873cc0db:0x227d185e8e05e363!8m2!3d37.7827092!4d-122.3935642!16s%2Fg%2F11b76gvzx1?authuser=0&entry=ttu&g_ep=EgoyMDI1MDEwNy4wIKXMDSoASAFQAw%3D%3D"><span class="text-Tonic underline underline-offset-2">512 2nd Street, Suite 200 San Francisco, CA 94107, États-Unis</span></a></p>
+        <p>Contact : <a href="mailto:support@netlify.com"><span class="text-Tonic underline underline-offset-2">support@netlify.com</span></a></p>
     </section>
     <section>
         <h2>Propriété<br/>intellectuelle</h2>
         <p>Les illustrations des personnages sur la page d'accueil sont réalisées par l'artiste ElevenZM, dont les droits d'auteur sont réservés. Toute reproduction ou utilisation non autorisée est interdite. L’utilisation de ses illustrations se fait sur la mention de l’artiste dans nos mentions légales.</p>
         <p>Lien de sa page sur Deviant Art :</p>
-        <p><a href="https://www.deviantart.com/elevenzm/gallery">https://www.deviantart.com/elevenzm/gallery.</a></p>
+        <p><a href="https://www.deviantart.com/elevenzm/gallery"><span class="text-Tonic underline underline-offset-2">https://www.deviantart.com/elevenzm/gallery.</span></a></p>
     </section>
 </template>
