28277bba8ce8489e0f90543990a18742796d8877
Barder_Lucas <bardey.estelle1@gmail.com>
2025-01-17 14:01:12 +0100

    ajout des grilles

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,37 @@ export default {
       "fugaz-one": "Fugaz One",
       "gothic-a1": "Gothic A1"
      },
-    extend: {}
+    extend: {
+      gridTemplateColumns: {
+        // Simple 16 column grid
+        'principal': 'repeat(12, 1fr)',
+        '12': 'repeat(12, 1fr)',
+        '11': 'repeat(11, 1fr)',
+        '10': 'repeat(10, 1fr)',
+        '9': 'repeat(9, 1fr)',
+        '8': 'repeat(8, 1fr)',
+        '7': 'repeat(7, 1fr)',
+        '6': 'repeat(6, 1fr)',
+        '5': 'repeat(5, 1fr)',
+        '4': 'repeat(4, 1fr)',
+        '3': 'repeat(3, 1fr)',
+        '2': 'repeat(2, 1fr)',
+        '1': 'repeat(1, 1fr)',
+        'telprincipal':'repeat(12, 20px)',
+        'tel12': 'repeat(12, 20px)',
+        'tel11': 'repeat(11, 20px)',
+        'tel10': 'repeat(10, 20px)',
+        'tel9': 'repeat(9, 20px)',
+        'tel8': 'repeat(8, 20px)',
+        'tel7': 'repeat(7, 20px)',
+        'tel6': 'repeat(6, 20px)',
+        'tel5': 'repeat(5, 20px)',
+        'tel4': 'repeat(4, 20px)',
+        'tel3': 'repeat(3, 20px)',
+        'tel2': 'repeat(2, 20px)',
+        'tel1': 'repeat(1, 20px)',
+      }
+    },
   },
   plugins: []
 }
