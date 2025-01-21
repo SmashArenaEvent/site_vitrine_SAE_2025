d6e19374513169cdf3d9ed71cfee59bb5652fc09
Barder_Lucas <bardey.estelle1@gmail.com>
2025-01-21 11:41:34 +0100

    header responsive fonctionnel

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -15,29 +15,29 @@
     const activeMenu = ref(false)
 </script>
 <template>
-    <header class="grille h-20 lg:h-52 w-full justify-between font-fugaz-one text-xs">
+    <header class="grille h-20 lg:h-52 w-full justify-between font-fugaz-one text-xs fixed z-50">
         <RouterLink to="/" class="col-span-3 lg:col-span-2 my-auto">
-            <Logo class="h-10 lg:h-20" alt="Logo Smash Arena Event"/>
+            <Logo class="h-10 lg:h-20 relative z-50" alt="Logo Smash Arena Event"/>
         </RouterLink>
-        <nav class="hidden lg:flex gap-5 lg:text-3xl justify-between lg:col-span-7 items-center" :class="{'!flex':activeMenu}">
-            <RouterLink to="/regles">
-                <button>
-                    <p class="uppercase">Règles</p>
+        <nav class="hidden lg:flex gap-5 lg:gap-10 lg:text-3xl justify-center lg:justify-between lg:col-span-7 items-center" :class="{'!flex flex-col lg:flex-row absolute lg:static left-0 bg-Noir w-screen lg:w-auto h-screen lg:h-auto z-40':activeMenu}">
+            <RouterLink to="/regles" class="relative">
+                <button class="bg-Tonic lg:bg-transparent h-12 lg:h-auto -skew-x-[25deg] lg:skew-x-0">
+                    <p class="text-Noir lg:text-Blanc font-fugaz-one uppercase text-xl lg:text-3xl px-6 lg:px-0 skew-x-[25deg] lg:skew-x-0">Règles</p>
                 </button>
             </RouterLink>
             <RouterLink to="/faq">
-                <button>
-                    <p class="uppercase">FAQ</p>
+                <button class="bg-Tonic lg:bg-transparent h-12 lg:h-auto -skew-x-[25deg] lg:skew-x-0">
+                    <p class="text-Noir lg:text-Blanc font-fugaz-one uppercase text-xl lg:text-3xl px-6 lg:px-0 skew-x-[25deg] lg:skew-x-0">FAQ</p>
                 </button>
             </RouterLink>
             <RouterLink to="/aPropos">
-                <button>
-                    <p class="uppercase">à propos</p>
+                <button class="bg-Tonic lg:bg-transparent h-12 lg:h-auto -skew-x-[25deg] lg:skew-x-0">
+                    <p class="text-Noir lg:text-Blanc font-fugaz-one uppercase text-xl lg:text-3xl px-6 lg:px-0 skew-x-[25deg] lg:skew-x-0">à propos</p>
                 </button>
             </RouterLink>
             <RouterLink to="/contact">
-                <button>
-                    <p class="uppercase">Contact</p>
+                <button class="bg-Tonic lg:bg-transparent h-12 lg:h-auto -skew-x-[25deg] lg:skew-x-0">
+                    <p class="text-Noir lg:text-Blanc font-fugaz-one uppercase text-xl lg:text-3xl px-6 lg:px-0 skew-x-[25deg] lg:skew-x-0">Contact</p>
                 </button>
             </RouterLink>
         </nav>
@@ -57,7 +57,7 @@
         </nav>
         <button class="col-span-6 col-start-7 lg:hidden">
             <Menu alt="Ouvrir le menu du header" class="block lg:hidden ml-auto" @click="activeMenu =! activeMenu" :class="{'!hidden':activeMenu}"/>
-            <Croix alt="Fermer le menu du header" class="hidden ml-auto" @click="activeMenu =! activeMenu" :class="{'!block':activeMenu, '!lg:hidden':activeMenu}"/>
+            <Croix alt="Fermer le menu du header" class="hidden ml-auto z-50 w-[30px]" @click="activeMenu =! activeMenu" :class="{'!block':activeMenu, '!relative':activeMenu}"/>
         </button>
     </header>
 </template>
--- src/components/icons/Croix.vue
+++ src/components/icons/Croix.vue
@@ -1,7 +1,7 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-<svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-<rect width="25.4557" height="2.82841" rx="1" transform="matrix(0.707104 0.70711 -0.707104 0.70711 12 10)" fill="#F5F5F5"/>
-<rect width="25.4557" height="2.82841" rx="1" transform="matrix(-0.707104 0.70711 -0.707104 -0.70711 30 12)" fill="#F5F5F5"/>
+<svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect width="38.1836" height="4.24262" rx="1" transform="matrix(0.707104 0.70711 -0.707104 0.70711 3 0)" fill="#F5F5F5"/>
+<rect width="38.1836" height="4.24262" rx="1" transform="matrix(-0.707104 0.70711 -0.707104 -0.70711 30 3)" fill="#F5F5F5"/>
 </svg>
 </template>
