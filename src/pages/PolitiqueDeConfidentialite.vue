aba771a465472a8982fd595cc8cbbfc79aab8136
Barder_Lucas <bardey.estelle1@gmail.com>
2025-01-24 14:45:30 +0100

    Politique e confidentialité et page 404 responsive

--- src/pages/PolitiqueDeConfidentialite.vue
+++ src/pages/PolitiqueDeConfidentialite.vue
@@ -3,41 +3,41 @@
     
 </script>
 <template>
-    <h1 class="font-fugaz-one uppercase text-4xl lg:text-6xl"><span class="text-Tonic">P</span>olitique <span class="text-Tonic">d</span>e <span class="text-Tonic">c</span>onfidentialité</h1>
-    <section>
-        <h2 class="font-fugaz-one uppercase text-3xl lg:text-5xl"><span class="text-Tonic">Données</span> Personneles et RGPD</h2>
-        <p class="text-xl">Les visiteurs du site peuvent s'inscrire en tant que joueur ou bénévole pour le tournoi. Les données personnelles collectées lors de l'inscription, telles que le nom, l'adresse e-mail, et d'autres informations pertinentes, seront utilisées dans le seul but de gérer la participation au tournoi et de contacter les participants.</p>
-        <p class="text-xl">Un registre pour le traitement des données personnelles conforme au RGPD sera maintenu. Les utilisateurs ont le droit d'accéder, de rectifier, de supprimer ou de demander la portabilité de leurs données personnelles. Pour exercer ces droits, les utilisateurs peuvent envoyer un e-mail à l'adresse spécifiée sur la page contact : <a href="mailto:smasharenaevent@gmail.com"><span class="text-Tonic underline underline-offset-2">smasharenaevent@gmail.com.</span></a></p>
+    <h1 class="col-span-12 font-fugaz-one uppercase text-4xl lg:text-6xl mt-8 lg:pb-12"><span class="text-Tonic">P</span>olitique <span class="text-Tonic">d</span>e <span class="text-Tonic">c</span>onfidentialité</h1>
+    <section class="col-span-12">
+        <h2 class="font-fugaz-one uppercase text-3xl lg:text-5xl mb-6 lg:mb-12"><span class="text-Tonic">Données</span> Personneles et RGPD</h2>
+        <p class="text-xl leading-6 lg:leading-10">Les visiteurs du site peuvent s'inscrire en tant que joueur ou bénévole pour le tournoi. Les données personnelles collectées lors de l'inscription, telles que le nom, l'adresse e-mail, et d'autres informations pertinentes, seront utilisées dans le seul but de gérer la participation au tournoi et de contacter les participants.</p>
+        <p class="text-xl leading-6 lg:leading-10">Un registre pour le traitement des données personnelles conforme au RGPD sera maintenu. Les utilisateurs ont le droit d'accéder, de rectifier, de supprimer ou de demander la portabilité de leurs données personnelles. Pour exercer ces droits, les utilisateurs peuvent envoyer un e-mail à l'adresse spécifiée sur la page contact : <a href="mailto:smasharenaevent@gmail.com"><span class="text-Tonic underline underline-offset-2">smasharenaevent@gmail.com.</span></a></p>
     </section>
-    <section>
-        <h2 class="font-fugaz-one uppercase text-3xl lg:text-5xl"><span class="text-Tonic">Inscriptions</span> en tant que joueur ou bénévole</h2>
-        <p class="text-xl">Les inscriptions en tant que joueur utiliseront l'API de tournois Challonge pour stocker les données des joueurs. Les inscriptions en tant que bénévole généreront des données via un formulaire sur le site Jotform. En s'inscrivant, les participants acceptent que ces données soient utilisées dans le cadre du tournoi. Les données ne seront pas partagées, vendues ou louées à des tiers.</p>
+    <section class="col-span-12">
+        <h2 class="font-fugaz-one uppercase text-3xl lg:text-5xl mt-12 mb-6 lg:mt-24 lg:mb-12"><span class="text-Tonic">Inscriptions</span> en tant que joueur ou bénévole</h2>
+        <p class="text-xl leading-6 lg:leading-10">Les inscriptions en tant que joueur utiliseront l'API de tournois Challonge pour stocker les données des joueurs. Les inscriptions en tant que bénévole généreront des données via un formulaire sur le site Jotform. En s'inscrivant, les participants acceptent que ces données soient utilisées dans le cadre du tournoi. Les données ne seront pas partagées, vendues ou louées à des tiers.</p>
     </section>
-    <section>
-        <h2 class="font-fugaz-one uppercase text-3xl lg:text-5xl"><span class="text-Tonic">Utilisation</span> des données collectées</h2>
-        <p class="text-xl">Les données collectées seront utilisées pour gérer les inscriptions au tournoi, communiquer des informations relatives à l'événement, et faciliter la participation des joueurs et bénévoles. Les données ne seront pas utilisées à des fins commerciales non liées à l'événement.</p>
+    <section class="col-span-12">
+        <h2 class="font-fugaz-one uppercase text-3xl lg:text-5xl mt-12 mb-6 lg:mt-24 lg:mb-12"><span class="text-Tonic">Utilisation</span> des données collectées</h2>
+        <p class="text-xl leading-6 lg:leading-10">Les données collectées seront utilisées pour gérer les inscriptions au tournoi, communiquer des informations relatives à l'événement, et faciliter la participation des joueurs et bénévoles. Les données ne seront pas utilisées à des fins commerciales non liées à l'événement.</p>
     </section>
-    <section>
-        <h2 class="font-fugaz-one uppercase text-3xl lg:text-5xl"><span class="text-Tonic">Sécurité</span> des données</h2>
-        <p class="text-xl">Nous nous engageons à assurer la sécurité des données personnelles collectées et à mettre en place des mesures appropriées pour protéger ces données contre tout accès non autorisé, perte ou divulgation.</p>
+    <section class="col-span-12">
+        <h2 class="font-fugaz-one uppercase text-3xl lg:text-5xl mt-12 mb-6 lg:mt-24 lg:mb-12"><span class="text-Tonic">Sécurité</span> des données</h2>
+        <p class="text-xl leading-6 lg:leading-10">Nous nous engageons à assurer la sécurité des données personnelles collectées et à mettre en place des mesures appropriées pour protéger ces données contre tout accès non autorisé, perte ou divulgation.</p>
     </section>
-    <section>
-        <h2 class="font-fugaz-one uppercase text-3xl lg:text-5xl"><span class="text-Tonic">Vos</span> Droits</h2>
-        <p class="text-xl">Les utilisateurs ont le droit d'accéder, de rectifier, de supprimer, ou de demander la portabilité de leurs données personnelles. Pour exercer ces droits, les utilisateurs peuvent envoyer un e-mail à l'adresse spécifiée sur notre page contact et demander aux organisateurs à réaliser un changement ou une suppression de leurs données personnelles.</p>
+    <section class="col-span-12">
+        <h2 class="font-fugaz-one uppercase text-3xl lg:text-5xl mt-12 mb-6 lg:mt-24 lg:mb-12"><span class="text-Tonic">Vos</span> Droits</h2>
+        <p class="text-xl leading-6 lg:leading-10">Les utilisateurs ont le droit d'accéder, de rectifier, de supprimer, ou de demander la portabilité de leurs données personnelles. Pour exercer ces droits, les utilisateurs peuvent envoyer un e-mail à l'adresse spécifiée sur notre page contact et demander aux organisateurs à réaliser un changement ou une suppression de leurs données personnelles.</p>
     </section>
-    <section>
-        <h2 class="font-fugaz-one uppercase text-3xl lg:text-5xl"><span class="text-Tonic">Modifications</span> de la politique de confidentialité</h2>
-        <p class="text-xl">Nous nous réservons le droit de modifier cette politique de confidentialité à tout moment. Les utilisateurs seront informés de ces modifications via nos réseaux sociaux.</p>
-        <p class="text-xl">Facebook : <a href="https://www.facebook.com/profile.php?id=61553864250828"><span class="text-Tonic underline underline-offset-2">https://www.facebook.com/profile.php?id=61553864250828.</span></a></p>
-        <p class="text-xl">Twitter : <a href=" https://x.com/SmashArenaEvent"><span class="text-Tonic underline underline-offset-2">https://x.com/SmashArenaEvent.</span></a></p>
-        <p class="text-xl">Instagram : <a href="https://www.instagram.com/smash_arena_event/"><span class="text-Tonic underline underline-offset-2">https://www.instagram.com/smash_arena_event/.</span></a></p>
+    <section class="col-span-12">
+        <h2 class="font-fugaz-one uppercase text-3xl lg:text-5xl mt-12 mb-6 lg:mt-24 lg:mb-12"><span class="text-Tonic">Modifications</span> de la politique de confidentialité</h2>
+        <p class="text-xl leading-6 lg:leading-10">Nous nous réservons le droit de modifier cette politique de confidentialité à tout moment. Les utilisateurs seront informés de ces modifications via nos réseaux sociaux.</p>
+        <p class="text-xl leading-6 lg:leading-10 mt-8 lg:mt-12">Facebook : <a href="https://www.facebook.com/profile.php?id=61553864250828"><span class="text-Tonic underline underline-offset-2">https://www.facebook.com/profile.php?id=61553864250828.</span></a></p>
+        <p class="text-xl leading-6 lg:leading-10">Twitter : <a href=" https://x.com/SmashArenaEvent"><span class="text-Tonic underline underline-offset-2">https://x.com/SmashArenaEvent.</span></a></p>
+        <p class="text-xl leading-6 lg:leading-10">Instagram : <a href="https://www.instagram.com/smash_arena_event/"><span class="text-Tonic underline underline-offset-2">https://www.instagram.com/smash_arena_event/.</span></a></p>
     </section>
-    <section>
-        <h2 class="font-fugaz-one uppercase text-3xl lg:text-5xl"><span class="text-Tonic">Politique</span> de Challonge (API utilisée pour les joueurs)</h2>
-        <p class="text-xl">Challonge est un service tiers utilisé pour gérer les données des joueurs. La politique de confidentialité et les conditions d'utilisation de Challonge peuvent être consultées sur leur site web : <a href="https://challonge.com/privacy_policy"><span class="text-Tonic underline underline-offset-2">https://challonge.com/privacy_policy.</span></a></p>
+    <section class="col-span-12">
+        <h2 class="font-fugaz-one uppercase text-3xl lg:text-5xl mt-12 mb-6 lg:mt-24 lg:mb-12"><span class="text-Tonic">Politique</span> de Challonge (API utilisée pour les joueurs)</h2>
+        <p class="text-xl leading-6 lg:leading-10">Challonge est un service tiers utilisé pour gérer les données des joueurs. La politique de confidentialité et les conditions d'utilisation de Challonge peuvent être consultées sur leur site web : <a href="https://challonge.com/privacy_policy"><span class="text-Tonic underline underline-offset-2">https://challonge.com/privacy_policy.</span></a></p>
     </section>
-    <section>
-        <h2 class="font-fugaz-one uppercase text-3xl lg:text-5xl"><span class="text-Tonic">Politique</span> de Jotform (site utilisé pour les bénévoles)</h2>
-        <p class="text-xl">Jotform est un service tiers utilisé pour générer des données à partir des formulaires des bénévoles. La politique de confidentialité et les conditions d'utilisation de Jotform peuvent être consultées sur leur site web :<br/><a href="https://www.jotform.com/privacy"><span class="text-Tonic underline underline-offset-2">https://www.jotform.com/privacy.</span></a></p>
+    <section class="col-span-12">
+        <h2 class="font-fugaz-one uppercase text-3xl lg:text-5xl mt-12 mb-6 lg:mt-24 lg:mb-12"><span class="text-Tonic">Politique</span> de Jotform (site utilisé pour les bénévoles)</h2>
+        <p class="text-xl leading-6 lg:leading-10">Jotform est un service tiers utilisé pour générer des données à partir des formulaires des bénévoles. La politique de confidentialité et les conditions d'utilisation de Jotform peuvent être consultées sur leur site web :<br/><a href="https://www.jotform.com/privacy"><span class="text-Tonic underline underline-offset-2">https://www.jotform.com/privacy.</span></a></p>
     </section>
 </template>
--- src/pages/[...404].vue
+++ src/pages/[...404].vue
@@ -3,18 +3,18 @@
     
 </script>
 <template>
-    <article class="lg:flex">
-        <section>
-            <h1 class="font-fugaz-one uppercase text-4xl text-center"><span class="text-Tonic">E</span>rreur <span class="text-Tonic">404</span></h1>
-            <p class="text-center">Même Donkey Kong n’a su retrouver la page.<br/>Celle-ci doit être actuellement introuvable.</p>
-            <RouterLink to="/" class="hidden lg:block">
+    <article class="flex flex-col items-center col-span-12 User-View justify-center lg:col12 py-auto lg:content-center lg:pb-[10vh]">
+        <section class="lg:col-start-2 col-span-4 lg:my-auto">
+            <h1 class="font-fugaz-one uppercase text-4xl lg:text-6xl text-center leading-[56px] lg:leading-normal lg:text-left"><span class="text-Tonic">E</span>rreur <span class="text-Tonic">404</span></h1>
+            <p class="text-center mt-5 my-12 lg:text-left">Même Donkey Kong n’a su retrouver la page.<br/>Celle-ci doit être actuellement introuvable.</p>
+            <RouterLink to="/" class="hidden lg:block ml-[13px]">
                 <button class="bg-Tonic h-12 -skew-x-[25deg]">
                     <p class="font-fugaz-one uppercase text-Noir px-6 skew-x-[25deg]">Retourner à l'accueil</p>
                 </button>
             </RouterLink>
         </section>
-        <img src="/src/components/img/DonkeyKong404.png" alt="Image de donkey kong">
-        <RouterLink to="/" class="lock lg:hidden">
+        <img class="lg:col-start-7 lg:col-span-6" src="/src/components/img/DonkeyKong404.png" alt="Image de donkey kong">
+        <RouterLink to="/" class="lock lg:hidden mt-8">
             <button class="bg-Tonic h-12 -skew-x-[25deg]">
                 <p class="font-fugaz-one uppercase text-Noir px-6 skew-x-[25deg]">Retourner à l'accueil</p>
             </button>
