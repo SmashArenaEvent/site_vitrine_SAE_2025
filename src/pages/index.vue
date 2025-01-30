d399f032f8d67a15ab1c26177afa2643891f9981
Barder_Lucas <bardey.estelle1@gmail.com>
2025-01-30 17:13:40 +0100

    Version place holder de la section inscription

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -202,29 +202,37 @@ onMounted(() => {
     >
       <span class="text-Tonic">Tu veux participer ? </span>Inscris-toi !
     </h2>
-    <nav class="col-span-12 flex flex-col lg:flex-row mt-6 lg:mt-12">
-      <div class="w-full bg-center bg-cover bg-[url('/src/components/img/WallPaperJoueur.png')]">
-        <div class="h-52 lg:h-[608px] w-full bg-Noir_Vert hover:bg-transparent col12 content-center transition-all duration-300">
-          <h3 class="col-start-2 col-span-10 font-fugaz-one uppercase text-xl lg:text-5xl text-Tonic h-7 lg:h-16">Comme joueur</h3>
-          <p class="col-start-2 col-span-10 lg:text-xl mt-1">et participe au tournoi !</p>
-          <RouterLink class="col-start-2 col-span-10 mt-6 lg:mt-20 ml-[13px]" to="#">
-            <button class="bg-Tonic h-12 lg:h-20 -skew-x-[25deg]">
-              <p class="text-Noir font-fugaz-one uppercase text-xs lg:text-xl px-6 lg:px-9 skew-x-[25deg]">Je m’inscris</p>
-            </button>
-          </RouterLink>
+    <nav class="col-span-12 flex flex-col lg:flex-row overflow-hidden mt-6 lg:mt-12">
+      <div class="hover:z-10 relative w-full bg-center bg-cover bg-[url('/src/components/img/WallPaperJoueur.png')] overflow-hidden hover:scale-110 transition-all duration-1000">
+        <div class="h-52 lg:h-[608px] w-full bg-Noir_Vert hover:bg-transparent transition-all duration-1000 overflow-hidden">
+          <div class="h-full w-full col12 content-center hover:scale-[0.91] transition-all duration-1000">
+            <h3 class="col-start-2 col-span-10 font-fugaz-one uppercase text-xl lg:text-5xl text-Tonic h-7 lg:h-16">Comme joueur</h3>
+            <p class="col-start-2 col-span-10 lg:text-xl mt-1">et participe au tournoi !</p>
+            <RouterLink class=" col-start-2 col-span-10 mt-6 lg:mt-20 ml-[13px]" to="#">
+              <div class="bg-Noir w-fit -skew-x-[25deg]">
+                <button class="bg-Tonic h-12 lg:h-20 hover:opacity-60">
+                  <p class="text-Noir font-fugaz-one uppercase text-xs lg:text-xl px-6 lg:px-9 skew-x-[25deg]">Je m’inscris</p>
+                </button>
+              </div>
+            </RouterLink>
+          </div>
         </div>
       </div>
-      <div class="w-full bg-center bg-cover bg-[url('/src/components/img/WallPaperBenevole.png')]">
-        <div class="h-52 lg:h-[608px] w-full bg-Noir_Vert hover:bg-transparent col12 content-center transition-all duration-300">
-          <h3 class="text-right col-start-2 col-span-10 font-fugaz-one uppercase text-xl lg:text-5xl text-Tonic h-7 lg:h-16">
-            Comme Bénévole
-          </h3>
-          <p class="text-right col-start-2 col-span-10 lg:text-xl mt-1">et viens nous aider à gérer l’évènement !</p>
-          <RouterLink class="ml-auto col-start-2 col-span-10 mt-6 lg:mt-20 mr-[13px]" to="#">
-            <button class="bg-Tonic h-12 lg:h-20 -skew-x-[25deg]">
-              <p class="text-Noir font-fugaz-one uppercase text-xs lg:text-xl px-6 lg:px-9 skew-x-[25deg]">Je m’inscris</p>
-            </button>
-          </RouterLink>
+      <div class="hover:z-10 relative w-full bg-center bg-cover bg-[url('/src/components/img/WallPaperBenevole.png')] overflow-hidden hover:scale-110 transition-all duration-1000">
+        <div class="lg:z-10 h-52 lg:h-[608px] w-full bg-Noir_Vert hover:bg-transparent transition-all duration-1000 overflow-hidden">
+          <div class="h-full col12 content-center hover:scale-[0.91] transition-all duration-1000">
+            <h3 class="text-right col-start-2 col-span-10 font-fugaz-one uppercase text-xl lg:text-5xl text-Tonic h-7 lg:h-16">
+              Comme Bénévole
+            </h3>
+            <p class="text-right col-start-2 col-span-10 lg:text-xl mt-1">et viens nous aider à gérer l’évènement !</p>
+            <RouterLink class="ml-auto col-start-2 col-span-10 mt-6 lg:mt-20 mr-[13px]" to="#">
+              <div class="bg-Noir w-fit -skew-x-[25deg]">
+                <button class="bg-Tonic h-12 lg:h-20 hover:opacity-60">
+                  <p class="text-Noir font-fugaz-one uppercase text-xs lg:text-xl px-6 lg:px-9 skew-x-[25deg]">Je m’inscris</p>
+                </button>
+              </div>
+            </RouterLink>
+          </div>
         </div>
       </div>
     </nav>
