13aed5f3a84be570454e44b6ba9624da8b46fe4e
Barder_Lucas <bardey.estelle1@gmail.com>
2025-02-09 18:29:43 +0100

    aria label footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -15,11 +15,12 @@ const route = useRoute()
     class="font-gothic-a1 text-xs grille pt-7 lg:pt-5 pb-5 lg:pb-4 bg-Noir text-Blanc -mt-64 lg:-mt-[260px] w-full overflow-x-hidden relative z-10"
   >
     <div class="w-full col-span-12 col12">
-      <RouterLink to="/" class="col-start-1 col-span-3 lg:col-span-2" onclick="window.scrollTo(0, 0);">
-      <Logo
-        class="h-10 lg:h-20 lg:mt-8 hover:opacity-60 duration-200"
-        alt="Logo Smash Arena Event"
-      />
+      <RouterLink to="/" aria-label="Retour à la page d'accueil" class="col-start-1 col-span-3 lg:col-span-2 w-fit h-fit" onclick="window.scrollTo(0, 0);">
+        <Logo
+          aria-label="logo du site"
+          class="h-10 lg:h-20 lg:mt-8 hover:opacity-60 duration-200"
+          alt="Logo Smash Arena Event"
+        />
       </RouterLink>
       <nav
         class="flex flex-col lg:text-base lg:h-36 lg:justify-center col-span-2 h- mt-3 lg:mt-0 gap-2 lg:gap-3"
@@ -33,18 +34,20 @@ const route = useRoute()
         class="flex flex-col lg:flex-row grid-cols-2 gap-8 col-start-9 col-span-4 lg:h-36 items-center ml-auto"
       >
         <div class="flex gap-8">
-          <a class="w-9 lg:w-10 h-9 lg:h-10 hover:opacity-60 duration-200" href="https://www.instagram.com/smash_arena_event/"
+          <a aria-label="Instagram" class="w-9 lg:w-10 h-9 lg:h-10 hover:opacity-60 duration-200" href="https://www.instagram.com/smash_arena_event/"
             ><Insta alt="Instagram"
           /></a>
-          <a class="w-9 lg:w-10 h-9 lg:h-10 hover:opacity-60 duration-200" href="https://twitter.com/SmashArenaEvent"
+          <a aria-label="Twitter" class="w-9 lg:w-10 h-9 lg:h-10 hover:opacity-60 duration-200" href="https://twitter.com/SmashArenaEvent"
             ><X alt="X"
           /></a>
         </div>
         <div class="flex gap-8">
-          <a class="w-9 lg:w-10 h-9 lg:h-10 hover:opacity-60 duration-200" href="https://discord.gg/6zFJf9WXy2"
+          <a aria-label="Discord" class="w-9 lg:w-10 h-9 lg:h-10 hover:opacity-60 duration-200" href="https://discord.gg/6zFJf9WXy2"
             ><Discord alt="Discord"
           /></a>
-          <a class="w-9 lg:w-10 h-9 lg:h-10 hover:opacity-60 duration-200" href="#"><Youtube alt="Youtube" /></a>
+          <a aria-label="Youtube" class="w-9 lg:w-10 h-9 lg:h-10 hover:opacity-60 duration-200" href="#">
+            <Youtube alt="Youtube" />
+          </a>
         </div>
       </nav>
     </div>
