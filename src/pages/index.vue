e0c4341709ac6304fbeb213494307a56f5ffb69c
Barder_Lucas <bardey.estelle1@gmail.com>
2025-02-10 15:03:57 +0100

    Mise à jour des inscriptions et avancé des méta donnée

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -232,65 +232,37 @@
     >
       <span class="text-Tonic">Tu veux participer ? </span>Inscris-toi !
     </h2>
-    <nav class="col-span-12 flex flex-col lg:flex-row overflow-hidden mt-6 lg:mt-12 relative z-10">
-      <div
-      class="hover:z-10 relative w-full bg-center bg-cover bg-[url('/src/components/img/WallPaperJoueur.webp')] overflow-hidden hover:scale-110 transition-all duration-1000" loading="lazy"
-      >
-        <div
-        class="h-52 lg:h-[608px] w-full bg-Noir_Vert hover:bg-transparent transition-all duration-1000 overflow-hidden"
-        >
-          <div
-          class="h-full w-full col12 content-center hover:scale-[0.91] transition-all duration-1000"
-          >
-            <h3
-            class="col-start-2 col-span-10 font-fugaz-one uppercase text-xl lg:text-5xl text-Tonic h-7 lg:h-16"
-            >
-              Comme joueur
-            </h3>
-            <p href="https://form.jotform.com/SmashArenaEvent/Inscription-Tournoi" class="col-start-2 col-span-10 lg:text-xl mt-1">et participe au tournoi !</p>
-            <a class="col-start-2 col-span-10 mt-6 lg:mt-20 ml-[13px]" onclick="window.scrollTo(0, 0);" to="/nouvel-adversaire">
-              <div class="bg-Noir w-fit -skew-x-[25deg]">
-                <button class="bg-Tonic h-12 lg:h-20 hover:opacity-60">
-                  <p
-                  class="text-Noir font-fugaz-one uppercase text-xs lg:text-xl px-6 lg:px-9 skew-x-[25deg]"
-                  >
-                    Je m’inscris
-                  </p>
-                </button>
-              </div>
-            </a>
-          </div>
+    <nav class="col-span-12 flex flex-col lg:flex-row mt-6 lg:mt-12 relative z-10 -mx-5 lg:-mx-[50px] h-[416px] lg:h-auto">
+        <div class="bg-center bg-cover bg-[url('/src/components/img/WallPaperJoueur.webp')] case_1 transition-all duration-1000 relative" loading="lazy">
+            <div class="h-full lg:h-[608px] w-full bg-Noir_Vert hover:bg-transparent transition-all duration-1000 px-5 hover:pl-10 lg:px-[50px] hover:lg:pl-[100px] flex flex-col justify-center">
+                <h3 class="font-fugaz-one uppercase text-xl lg:text-5xl text-Tonic h-7 lg:h-16">
+                    Comme joueur
+                </h3>
+                <p class="lg:text-xl mt-1">et participe au tournoi !</p>
+                <a class="mt-6 lg:mt-20 mr-[13px] bg-Noir w-fit -skew-x-[25deg]" href="https://form.jotform.com/SmashArenaEvent/Inscription-Tournoi" onclick="window.scrollTo(0, 0);">
+                    <button class="bg-Tonic h-12 lg:h-20 hover:opacity-60">
+                        <p class="text-Noir font-fugaz-one uppercase text-xs lg:text-xl px-6 lg:px-9 skew-x-[25deg]">
+                            Je m’inscris
+                        </p>
+                    </button>
+                </a>
+            </div>
         </div>
-      </div>
-      <div
-      class="hover:z-10 relative w-full bg-center bg-cover bg-[url('/src/components/img/WallPaperBenevole.webp')] overflow-hidden hover:scale-110 transition-all duration-1000" loading="lazy"
-      >
-        <div
-        class="lg:z-10 h-52 lg:h-[608px] w-full bg-Noir_Vert hover:bg-transparent transition-all duration-1000 overflow-hidden"
-        >
-          <div class="h-full col12 content-center hover:scale-[0.91] transition-all duration-1000">
-            <h3
-            class="text-right col-start-2 col-span-10 font-fugaz-one uppercase text-xl lg:text-5xl text-Tonic h-7 lg:h-16"
-            >
-              Comme Bénévole
-            </h3>
-            <p class="text-right col-start-2 col-span-10 lg:text-xl mt-1">
-              et viens nous aider à gérer l’évènement !
-            </p>
-            <a class="ml-auto col-start-2 col-span-10 mt-6 lg:mt-20 mr-[13px]" href="https://form.jotform.com/SmashArenaEvent/Inscription-Benevolat">
-              <div class="bg-Noir w-fit -skew-x-[25deg]">
-                <button class="bg-Tonic h-12 lg:h-20 hover:opacity-60">
-                  <p
-                  class="text-Noir font-fugaz-one uppercase text-xs lg:text-xl px-6 lg:px-9 skew-x-[25deg]"
-                  >
-                    Je m’inscris
-                  </p>
-                </button>
-              </div>
-            </a>
-          </div>
+        <div class="bg-center bg-cover bg-[url('/src/components/img/WallPaperBenevole.webp')] case_2 transition-all duration-1000 relative" loading="lazy">
+            <div class="h-full lg:h-[608px] w-full bg-Noir_Vert hover:bg-transparent transition-all duration-1000 px-5 hover:pr-10 lg:px-[50px] hover:lg:pr-[100px] flex flex-col justify-center items-end ">
+                <h3 class="font-fugaz-one uppercase text-xl lg:text-5xl text-Tonic h-7 lg:h-16">
+                    Comme Bénévole
+                </h3>
+                <p class="lg:text-xl mt-1">et viens nous aider à gérer l’évènement !</p>
+                <a class="mt-6 lg:mt-20 mr-[13px] bg-Noir w-fit -skew-x-[25deg]" href="https://form.jotform.com/SmashArenaEvent/Inscription-Benevolat" onclick="window.scrollTo(0, 0);">
+                    <button class="bg-Tonic h-12 lg:h-20 hover:opacity-60">
+                        <p class="text-Noir font-fugaz-one uppercase text-xs lg:text-xl px-6 lg:px-9 skew-x-[25deg]">
+                            Je m’inscris
+                        </p>
+                    </button>
+                </a>
+            </div>
         </div>
-      </div>
     </nav>
   </section>
   <!-- <section class="col-span-12 col12">
@@ -315,6 +287,20 @@
     <Formulaire />
   </section>
 </template>
+<style>
+    .case_1 {
+        flex: 5;
+    }
+    .case_1:hover {
+        flex: 7;
+    }
+    .case_2 {
+        flex: 5;
+    }
+    .case_2:hover {
+        flex: 7;
+    }
+</style>
 <script setup lang="ts">
 import { RouterLink } from 'vue-router'
 import { ref, onMounted } from 'vue'
--- src/pages/test.vue
+++ src/pages/test.vue
@@ -66,6 +66,67 @@
             </div>
         </div>
     </nav>
+    <!--Ancienne version-->
+    <nav class="col-span-12 flex flex-col lg:flex-row overflow-hidden mt-6 lg:mt-12 relative z-10">
+      <div
+      class="hover:z-10 relative w-full bg-center bg-cover bg-[url('/src/components/img/WallPaperJoueur.webp')] overflow-hidden hover:scale-110 transition-all duration-1000" loading="lazy"
+      >
+        <div
+        class="h-52 lg:h-[608px] w-full bg-Noir_Vert hover:bg-transparent transition-all duration-1000 overflow-hidden"
+        >
+          <div
+          class="h-full w-full col12 content-center hover:scale-[0.91] transition-all duration-1000"
+          >
+            <h3
+            class="col-start-2 col-span-10 font-fugaz-one uppercase text-xl lg:text-5xl text-Tonic h-7 lg:h-16"
+            >
+              Comme joueur
+            </h3>
+            <p href="https://form.jotform.com/SmashArenaEvent/Inscription-Tournoi" class="col-start-2 col-span-10 lg:text-xl mt-1">et participe au tournoi !</p>
+            <a class="col-start-2 col-span-10 mt-6 lg:mt-20 ml-[13px]" onclick="window.scrollTo(0, 0);" to="/nouvel-adversaire">
+              <div class="bg-Noir w-fit -skew-x-[25deg]">
+                <button class="bg-Tonic h-12 lg:h-20 hover:opacity-60">
+                  <p
+                  class="text-Noir font-fugaz-one uppercase text-xs lg:text-xl px-6 lg:px-9 skew-x-[25deg]"
+                  >
+                    Je m’inscris
+                  </p>
+                </button>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div
+      class="hover:z-10 relative w-full bg-center bg-cover bg-[url('/src/components/img/WallPaperBenevole.webp')] overflow-hidden hover:scale-110 transition-all duration-1000" loading="lazy"
+      >
+        <div
+        class="lg:z-10 h-52 lg:h-[608px] w-full bg-Noir_Vert hover:bg-transparent transition-all duration-1000 overflow-hidden"
+        >
+          <div class="h-full col12 content-center hover:scale-[0.91] transition-all duration-1000">
+            <h3
+            class="text-right col-start-2 col-span-10 font-fugaz-one uppercase text-xl lg:text-5xl text-Tonic h-7 lg:h-16"
+            >
+              Comme Bénévole
+            </h3>
+            <p class="text-right col-start-2 col-span-10 lg:text-xl mt-1">
+              et viens nous aider à gérer l’évènement !
+            </p>
+            <a class="ml-auto col-start-2 col-span-10 mt-6 lg:mt-20 mr-[13px]" href="https://form.jotform.com/SmashArenaEvent/Inscription-Benevolat">
+              <div class="bg-Noir w-fit -skew-x-[25deg]">
+                <button class="bg-Tonic h-12 lg:h-20 hover:opacity-60">
+                  <p
+                  class="text-Noir font-fugaz-one uppercase text-xs lg:text-xl px-6 lg:px-9 skew-x-[25deg]"
+                  >
+                    Je m’inscris
+                  </p>
+                </button>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
 </template>
 <style>
     .case_1 {
